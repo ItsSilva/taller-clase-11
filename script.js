d813d20f4009c9cb2ea82e2a9459b54599d64c35
ItsSilva <juanse909@hotmail.com>
2024-03-05 08:13:51 -0500

    taller-clase-11

---
+++ script.js
@@ -0,0 +1,7 @@
+const nom = prompt ("Ingrese su nombre");
+const apellido = prompt ("Ingrese su apellido");
+const añoDeNacimiento = parseInt (prompt("Ingrese su año de nacimiento"));
+const calcularEdad = 2024 - añoDeNacimiento;
+const diaDeNacimiento = prompt ("Ingrese su día de nacimiento");
+const mesDeNacimiento = prompt ("Ingrese su mes de nacimiento");
+console.log ("Mi nombre es", nom, apellido, "mi edad es", calcularEdad, "años y nací el", diaDeNacimiento, "de", mesDeNacimiento, "de", añoDeNacimiento)
