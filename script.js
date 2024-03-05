ee1b9f516bfcab7fd66e4f137d0007b7b12cca04
Juan S Silva <158085846+ItsSilva@users.noreply.github.com>
2024-03-05 08:14:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const añoDeNacimiento = parseInt (prompt("Ingrese su año de nacimiento"));
 const calcularEdad = 2024 - añoDeNacimiento;
 const diaDeNacimiento = prompt ("Ingrese su día de nacimiento");
 const mesDeNacimiento = prompt ("Ingrese su mes de nacimiento");
-console.log ("Mi nombre es", nom, apellido, "mi edad es", calcularEdad, "años y nací el", diaDeNacimiento, "de", mesDeNacimiento, "de", añoDeNacimiento)
+console.log ("Mi nombre es", nom, apellido, "mi edad es", calcularEdad, "años y nací el", diaDeNacimiento, "de", mesDeNacimiento, "de", añoDeNacimiento);
